51df7efd935e8805aa657770811e5db502c9e63c
Akshi1937 <90022380+Akshi1937@users.noreply.github.com>
2021-11-01 18:04:58 +0530

    Add files via upload

---
+++ types.js
@@ -0,0 +1,8 @@
+var x; // "x" has been declared but not initialised
+console.log(x);
+console.log("x is "+typeof x); 
+console.log(" 'x' was not initialised. So it's undefined. Undefined is a primitive value in java script.");
+// "y" was not even declared as a variable
+console.log("'y' was not even declared. Thus it is shown as not defined.");
+console.log(y); 
+
